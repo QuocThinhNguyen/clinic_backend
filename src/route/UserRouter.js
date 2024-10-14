f0564afbf1c791af7f946515e0ef161b3a3e611c
QuocThinhNguyen <nguyenquocthinh111@gmail.com>
2024-10-14 23:33:47 +0700

    CRUD clinic

--- src/route/UserRouter.js
+++ src/route/UserRouter.js
@@ -1,20 +1,25 @@
 import express from "express";
-import { createUserController,loginUserController,updateUserController, deleteUserController, getAllUserController, 
-    getDetailsUserController, refreshToken, logoutUserController, resetUserPasswordController, 
-    handleResetPasswordTokenController} from "../controllers/UserController.js";
-import { authMiddleware,authUserMiddleware } from "../middlewares/authMiddleware.js";
+import {
+    createUserController, loginUserController, updateUserController, deleteUserController, getAllUserController,
+    getDetailsUserController, refreshToken, logoutUserController, resetUserPasswordController,
+    handleResetPasswordTokenController, verifyUserController,
+    createAndSendOTPController
+} from "../controllers/UserController.js";
+import { authMiddleware, authUserMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post('/sign-up',createUserController)
-router.post('/sign-in',loginUserController)
+router.post('/sign-up', createAndSendOTPController)
+router.post('/verify-account/:token', verifyUserController)
+router.post('/sign-in', loginUserController)
 router.post('/logout', logoutUserController)
 router.post('/reset-password', resetUserPasswordController)
 router.get('/reset-password/:token', handleResetPasswordTokenController)
-router.put('/update-user/:id',updateUserController)
-router.delete('/delete-user/:id',authMiddleware ,deleteUserController)
-router.get('/get-all',authMiddleware, getAllUserController)
-router.get('/get-details/:id',authUserMiddleware , getDetailsUserController)
-router.post('/refresh_token',refreshToken)
+router.post('/create-user', createUserController)
+router.put('/update-user/:id', updateUserController)
+router.delete('/delete-user/:id', authMiddleware, deleteUserController)
+router.get('/get-all', authMiddleware, getAllUserController)
+router.get('/get-details/:id', authUserMiddleware, getDetailsUserController)
+router.post('/refresh_token', refreshToken)
 
 export default router
--- src/services/ClinicService.js
+++ src/services/ClinicService.js
@@ -34,8 +34,9 @@ const updateClinic = (id, data) => {
         try {
 
             const checkClinic = await clinic.findOne({
-                clinicID: id
+                clinicId: id
             })
+
             if (!checkClinic) {
                 resolve({
                     errCode: 2,
@@ -43,11 +44,31 @@ const updateClinic = (id, data) => {
                 });
             }
 
-            const updateClinic = await clinic.updateOne(
-                { clinicID: id },
+            await clinic.updateOne(
+                { clinicId: id },
                 data,
                 { new: true }
             )
+
+            resolve({
+                errCode: 0,
+                message: "Update clinic successfully"
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const getAllClinic = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const clinics = await clinic.find()
+            resolve({
+                errCode: 0,
+                message: "Get all clinic successfully",
+                data: clinics
+            })
 
         } catch (e) {
             reject(e)
@@ -55,7 +76,88 @@ const updateClinic = (id, data) => {
     })
 }
 
+const getDetailClinic = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const clinicData = await clinic.findOne({
+                clinicId: id
+            })
+            if (!clinicData) {
+                resolve({
+                    errCode: 2,
+                    errMessage: "Clinic is not defined"
+                })
+            }
+            resolve({
+                errCode: 0,
+                message: "Get clinic successfully",
+                data: clinicData
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const deleteClinic = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const findClinic = await clinic.findOne({
+                clinicId: id
+            })
+
+            if (!findClinic) {
+                resolve({
+                    errCode: 2,
+                    errMessage: "Clinic is not defined"
+                })
+            }
+
+            await clinic.deleteOne({
+                clinicId: id
+            })
+
+            resolve({
+                errCode: 0,
+                message: "Delete clinic successfully"
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const filterClinics = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            // Sử dụng biểu thức chính quy để tìm kiếm không chính xác
+            const query = {};
+            if (data.name) {
+                query.name = { $regex: data.name, $options: 'i' }; // 'i' để không phân biệt chữ hoa chữ thường
+            }
+            if (data.address) {
+                query.address = { $regex: data.address, $options: 'i' };
+            }
+            // Thêm các điều kiện tìm kiếm khác nếu cần
+
+            const clinics = await clinic.find(query);
+            resolve({
+                errCode: 0,
+                message: "Filter clinic successfully",
+                data: clinics
+            });
+
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
 export default {
     createClinic,
-    updateClinic
+    updateClinic,
+    getAllClinic,
+    getDetailClinic,
+    deleteClinic,
+    filterClinics
 }

aa79840816a53ec96a208ae1b141835689bc84a8
QuocThinhNguyen <nguyenquocthinh111@gmail.com>
2024-10-24 15:41:12 +0700

    updata doctor with image

---
+++ src/controllers/DoctorController.js
@@ -0,0 +1,68 @@
+import doctorInforService from '../services/DoctorService.js';
+
+const getDoctorInfor = async (req, res) => {
+    try {
+        const doctorId = req.params.id;
+        const info = await doctorInforService.getDoctorInfor(doctorId);
+        return res.status(200).json(info);
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            errCode: -1,
+            errMessage: "Error from server"
+        })
+    }
+}
+
+const getAllDoctor = async (req, res) => {
+    try {
+        const data = await doctorInforService.getAllDoctor();
+        return res.status(200).json(data);
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            errCode: -1,
+            errMessage: "Error from server"
+        })
+    }
+}
+
+const updateDoctorInfor = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const image = req.file ? `${req.file.filename}` : null;
+        const doctorData = {
+            ...req.body,
+            image
+        }
+
+        const data = await doctorInforService.updateDoctorInfor(id, doctorData);
+        return res.status(200).json(data);
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            errCode: -1,
+            errMessage: "Error from server"
+        })
+    }
+}
+
+const searchDoctor = async (req, res) => {
+    try {
+        const keyword = req.query.keyword.replace(/\s+/g, ' ').trim();
+        const result = await doctorInforService.searchDoctor(keyword);
+        return res.status(200).json(result);
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            errCode: -1,
+            errMessage: "Error from server"
+        })
+    }
+}
+export default {
+    getDoctorInfor,
+    updateDoctorInfor,
+    searchDoctor,
+    getAllDoctor
+}

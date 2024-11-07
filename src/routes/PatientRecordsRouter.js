20dcaf191f5d78ca3c0c975fd9ef54a0659eb5aa
QuocThinhNguyen <nguyenquocthinh111@gmail.com>
2024-11-07 23:34:35 +0700

    CRUD patient record

--- src/routes/PatientRecordsRouter.js
+++ src/routes/PatientRecordsRouter.js
@@ -5,5 +5,9 @@ import { authUserMiddleware } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.get("/", authUserMiddleware, patientRecordsController.getAllPatientRecords);
+router.get('/:id', patientRecordsController.getPatientRecordsById);
+router.post('/', patientRecordsController.createPatientRecord);
+router.put('/:id', patientRecordsController.updatePatientRecord);
+router.delete('/:id', patientRecordsController.deletePatientRecord)
 
 export default router;

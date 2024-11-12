79c635cd850839844f99c725aebb7e1a319b4a6a
T-Huy <lehuy099@gmail.com>
2024-11-12 21:23:31 +0700

    thêm cors update

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ import connectDB from "./config/connectDB.js";
 import upload from "./utils/fileUpload.js";
 import dotenv from "dotenv";
 import multer from "multer";
+import cors from "cors";
 dotenv.config();
 
 let app = express();
 
+const corsOptions = {
+  origin: `http://localhost:${process.env.FE_PORT}`, 
+  credentials: true,
+};
 
 // config app
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));

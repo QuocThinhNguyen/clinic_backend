20649588f0d861a927b39099dbd895c725f5c90f
QuocThinhNguyen <nguyenquocthinh111@gmail.com>
2024-11-21 21:04:07 +0700

    Xóa thư mục folder-name

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectMongoDB() {
     try {
-        await mongoose.connect("mongodb://localhost:27017/Clinic");
+        await mongoose.connect("mongodb://localhost:27017/ClinicData");
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
